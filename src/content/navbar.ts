d8a349f37db2440945ba32f53bf5477ef5f2f454
davi-p-oliveira-11 <dpo.dev11@gmail.com>
2025-03-13 20:46:41 -0300

    added navbar content

---
+++ src/content/navbar.ts
@@ -0,0 +1,15 @@
+export const navbarContent = {
+  logo: "",
+  links: [
+    { label: "Home", href: "/" },
+    { label: "Services", href: "#services" },
+    { label: "Features", href: "#clients" },
+    { label: "Product", href: "#product" },
+    { label: "Testimonial", href: "#testimonial" },
+    { label: "FAQ", href: "#faq" },
+  ],
+  buttons: [
+    { label: "Login", href: "/login" },
+    { label: "Signup", href: "/signup" },
+  ],
+};
