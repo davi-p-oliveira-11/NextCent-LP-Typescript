f3614ee0a770bdcd92c0ef7436e8076251710340
davi-p-oliveira-11 <dpo.dev11@gmail.com>
2025-03-13 21:58:28 -0300

    updated index.ts

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,6 +2,6 @@ import { Button } from "./Button";
 import { Card } from "./Card";
 import { Footer } from "./Footer";
 import { Input } from "./Input";
-import { Navbar } from "./Input";
+import { Navbar } from "./Navbar";
 
 export { Button, Card, Footer, Input, Navbar };
